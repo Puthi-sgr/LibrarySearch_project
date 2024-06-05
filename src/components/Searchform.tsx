e293df1553deddf5c7691ae15ae466f59a17f558
Puthi-sgr <chethaputhi@gmail.com>
2024-06-05 12:20:23 +0700

    Fix typescript error in context and handlesubmit stuff

--- src/components/Searchform.tsx
+++ src/components/Searchform.tsx
@@ -1,12 +1,32 @@
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
-
+import { AppProvider, useGLobalContext } from "./context";
+import { useRef, useEffect } from "react";
 export const SearchForm = () => {
+
+    const {setSearchTerm , setResultTitle} = useGLobalContext();
+    const searchText = useRef<HTMLInputElement>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => searchText.current?.focus(), [])
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        let inputValue = searchText.current?.value.trim();
+        if(inputValue?.replace(/[^\w\s]/gi, "").length === 0){
+            setSearchTerm("the lost world");
+            setResultTitle("Enter your favorite book title ...");
+        }else{
+            setSearchTerm(searchText?.current?.value ?? "")
+        }
+
+        navigate('/book');
+    }
     return(
         <div>
             <div className="container">
                 <div className="search-form-content">
-                    <form className="search-form">
+                    <form className="search-form" onSubmit={handleSubmit}>
                         <div className="search-form-element flex justify-between w-[650px] h-[42px]">
                             <input 
 
@@ -14,6 +34,7 @@ export const SearchForm = () => {
                                 type = "text" 
                                 className="w-[592px] h-[42px] rounded-md text-black pl-4" />
                             <button 
+                                onClick = {handleSubmit}
                                 className="w-12 rounded-md h-full bg-secondary flex items-center justify-center"
                                 type="submit">
                                 <FaMagnifyingGlass className="p size-4" />
--- src/components/context.tsx
+++ src/components/context.tsx
@@ -1,19 +1,69 @@
 import axios from 'axios';
-import React, {useState, useContext, useEffect, useCallback } from 'react';
+import React, {useState, useContext, useEffect, useCallback, ReactNode, FC, Dispatch, SetStateAction} from 'react';
 const URL = "https://openlibrary.org/search.json?title=";
-const AppContext = React.createContext({});
 
-export const AppProvider = ({children} : any) => {
-    const [searchTerm, setSearchTerm] = useState<string | null>("lord of the rings");
-    const [books, setBooks] = useState([]);
+interface GlobalContextType {
+  setSearchTerm: Dispatch<SetStateAction<string>>;
+  books: any[];
+  loading: boolean | null;
+  resultTitle: string;
+  setResultTitle: Dispatch<SetStateAction<string>>;
+}
+
+const AppContext = React.createContext<GlobalContextType | undefined>(undefined);
+
+export const AppProvider: FC<{children : ReactNode}> = ({children} ) => {
+    const [searchTerm, setSearchTerm] = useState<string>("the lost world");
+    const [books, setBooks] = useState<any[]>([]);
     const [loading, setLoading] = useState<boolean | null>(true);
-    const [resultTitle, setResultTitle] = useState<string | null>("");
+    const [resultTitle, setResultTitle] = useState<string>("");
+
+    const contextValue: GlobalContextType = {
+        setSearchTerm,
+        books,
+        loading,
+        resultTitle,
+        setResultTitle   
+    }
 
     const fetchBook = useCallback(async () => {
         setLoading(true);        
         try{
-            const response = await axios.get(URL);
-            const { docs }  = await response.data;
+            const response = await axios.get(`${URL}${searchTerm}`);
+            const { docs } = await response.data;
+
+            //if docs exists
+            if(docs){
+                const newBooks = docs.slice(0, 20).map((book: any) => {
+                    /*we extract these from each book we map*/
+                    const {key, 
+                            author_name, 
+                            cover_i, 
+                            edition_count, 
+                            first_publish_year, 
+                            title}  = book; 
+
+                    return {
+                        id: key,
+                        author: author_name,
+                        cover: cover_i,
+                        edition: edition_count,
+                        first_publish: first_publish_year,
+                        title: title
+                    }
+                });
+
+                setBooks(newBooks); //setting the array of books with book function that we have made in the above
+
+                if(newBooks.length() > 1){
+                    setResultTitle("This is your result");
+                }else{
+                    setBooks([]); //if the function return less than 1 book, set the array of book into an empty array;
+                    setResultTitle("No book found!");
+                }
+                setLoading(false);
+            }
+            console.log(docs);
         }catch(err){
             console.error(err);
         }                
@@ -23,19 +73,18 @@ export const AppProvider = ({children} : any) => {
         fetchBook();
     }, [searchTerm, fetchBook]);
 
+
     return (
-        <AppContext.Provider value={
-            {
-                setSearchTerm,
-                books,
-                loading,
-                resultTitle
-        }}>
+        <AppContext.Provider value={contextValue}>
             {children}
         </AppContext.Provider>
     )
 }
 
-export const useGLobalContext = () => {
-    return useContext(AppContext);
+export const useGLobalContext = (): GlobalContextType => {
+    const context = useContext(AppContext);
+    if(!context){
+        throw new Error('useGlobalContext must be used within a GlobalProvider');
+    }
+    return context;
 }
